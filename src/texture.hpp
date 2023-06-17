602895e5413baf23378961271a5d4f58868b7ef6
Iztok Bajcar <ib8548@student.uni-lj.si>
2023-06-17 15:30:03 +0200

    Updated docs

--- src/texture.hpp
+++ src/texture.hpp
@@ -8,11 +8,28 @@
 
 namespace blimp {
 
+    //! A texture that can be applied to a mesh.
     class Texture {
         public:
+            /** The default constructor.
+             * Creates a texture from an image file with the given filename.
+             * @param filename The filename of the image file
+            */
             Texture(std::string filename);
+
+            /** Returns the width of the texture.
+             * @return The width of the texture
+            */
             int getWidth();
+
+            /** Returns the height of the texture.
+             * @return The height of the texture
+            */
             int getHeight();
+
+            /** Returns the data of the texture as a char array.
+             * @return The data of the texture
+            */
             unsigned char* getData();
 
         private:
